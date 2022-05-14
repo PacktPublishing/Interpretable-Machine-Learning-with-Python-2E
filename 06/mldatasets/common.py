2118a81db3e5d8fa885c7ab506dff83f2c55b2e2
Sergio Masís <smasis@gmail.com>
2022-05-14 13:57:59 -0400

    Chapter 01-03, & 10

--- 06/mldatasets/common.py
+++ 06/mldatasets/common.py
@@ -406,11 +406,36 @@ def describe_cf_instance(X, explanation, class_names, cat_vars_ohe=None, categor
     else:
         print("\tNO COUNTERFACTUALS")
 
-def create_decision_plot(X, y, model, feature_index, feature_names, X_highlight, 
-                         filler_feature_values, filler_feature_ranges, ax=None):
+def create_decision_plot(X, y, model, feature_index, feature_names, X_highlight,\
+                         filler_feature_values, filler_feature_ranges=None, ax=None,\
+                         add_constant=True):
+    if feature_names is None:
+        feature_names=feature_index
+    if add_constant:
+        con=1
+    else:
+        con=0
+    if list(np.arange(X.shape[1]+con))!=list(filler_feature_values.keys()):
+        if add_constant:
+            fillers= {0:1}
+        else:
+            fillers= {}
+        fillers.update({X.columns.get_loc(key)+con:vals for key, vals in filler_feature_values.items()})
+        filler_feature_values = fillers
+    filler_feature_keys = list(filler_feature_values.keys())
+    if sum([True for k in feature_index if k not in filler_feature_keys])==2:
+        feature_index = [X.columns.get_loc(k)+con for k in feature_index if k not in filler_feature_keys]
     filler_values = dict((k, filler_feature_values[k]) for k in filler_feature_values.keys() if k not in feature_index)
+    X_arr = sm.add_constant(X).to_numpy()
+    if filler_feature_ranges is None:
+        filler_vals = np.array(list(filler_feature_values.values()))
+        filler_rangs = np.vstack([np.abs(np.amax(X_arr, axis=0) - filler_vals),\
+                                   np.abs(np.amin(X_arr, axis=0) - filler_vals)])
+        filler_rangemax = np.amax(filler_rangs, axis=0)
+        filler_rangemax = list(np.where(filler_rangemax==0, 1, filler_rangemax))
+        filler_feature_ranges = {i:v for i,v in enumerate(filler_rangemax)}
     filler_ranges = dict((k, filler_feature_ranges[k]) for k in filler_feature_ranges.keys() if k not in feature_index)
-    ax = plot_decision_regions(sm.add_constant(X).to_numpy(), y.to_numpy(), clf=model, 
+    ax = plot_decision_regions(X_arr, y.to_numpy(), clf=model, 
                           feature_index=feature_index,
                           X_highlight=X_highlight,
                           filler_feature_values=filler_values, 
